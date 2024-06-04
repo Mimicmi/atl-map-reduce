3131d5fd47c6ecc815091136e4aa4371498610a5
Simon Frezard <simon.frezard@gmail.com>
2024-06-04 10:10:27 +0200

    Feature(gitignore main): added main.py session of spark. Updated gitignore

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .master("local") \
+    .appName("atl-map-reduce") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+PATH = "Archive/applications_activity_per_user_per_hour_1.csv"
+
+df = spark.read.csv(PATH)
+
+df.show()
